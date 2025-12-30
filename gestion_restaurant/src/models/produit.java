61418b31c40770cb4041f7936f15b1e110193158
Boss <penielkouboumor@gmail.com>
2025-12-30 23:43:28 +0000

    suppression

--- gestion_restaurant/src/models/produit.java
+++ gestion_restaurant/src/models/produit.java
@@ -16,24 +16,17 @@ public class produit {
     private int seuil_alerte;
     private categorie categorie;
 
-<<<<<<< HEAD:gestion_restaurant/src/gestion_restaurant/produit.java
-    public produit(int id_produit, String nom, double prix_vente, int stock_actuel, int seuil_alerte, categorie categorie) {
-=======
-    public produit(int id_produit, String nom, double prix_vente, int stock_actuel, int seuil_alerte, categorie id_categorie) {
->>>>>>> 0de7652420a4a94abc7a5c3da8dac392f9065916:gestion_restaurant/src/models/produit.java
+    public produit(int id_produit, String nom, double prix_vente,
+                   int stock_actuel, int seuil_alerte, categorie categorie) {
         this.id_produit = id_produit;
         this.nom = nom;
         this.prix_vente = prix_vente;
         this.stock_actuel = stock_actuel;
         this.seuil_alerte = seuil_alerte;
-<<<<<<< HEAD:gestion_restaurant/src/gestion_restaurant/produit.java
         this.categorie = categorie;
-=======
-        this.categorie = id_categorie;
->>>>>>> 0de7652420a4a94abc7a5c3da8dac392f9065916:gestion_restaurant/src/models/produit.java
     }
-    
-    public int getId_produit(){
+
+    public int getId_produit() {
         return id_produit;
     }
 
@@ -52,12 +45,12 @@ public class produit {
     public int getSeuil_alerte() {
         return seuil_alerte;
     }
-    
+
     public categorie getCategorie() {
         return categorie;
     }
-    
-    public void setId_produit(int id_produit){
+
+    public void setId_produit(int id_produit) {
         this.id_produit = id_produit;
     }
 
@@ -77,19 +70,7 @@ public class produit {
         this.seuil_alerte = seuil_alerte;
     }
 
-<<<<<<< HEAD:gestion_restaurant/src/gestion_restaurant/produit.java
-    public void setId_categorie(categorie categorie) {
+    public void setCategorie(categorie categorie) {
         this.categorie = categorie;
-=======
-    public void setCategorie(categorie id_categorie) {
-        this.categorie = id_categorie;
->>>>>>> 0de7652420a4a94abc7a5c3da8dac392f9065916:gestion_restaurant/src/models/produit.java
     }
-    
-
-    
-    
-    
-
-    
 }
